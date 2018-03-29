8d52a1b7b2c1eef6d039cfae362e263cefd353cc
Alan Harnum <aharnum@ocadu.ca>
2018-03-29 16:32:52 -0400

    Initial commit.

---
+++ src/js/floe-zotero.js
@@ -0,0 +1,144 @@
+/* global fluid */
+
+(function ($, fluid) {
+
+    "use strict";
+
+    fluid.defaults("floe.zotero", {
+        gradeNames: ["fluid.component"],
+        zoteroConfig: {
+            baseUrl: "https://api.zotero.org/groups/2086760/items/",
+            limit: 50
+        },
+        components: {
+            zoteroItemsMetadata: {
+                type: "floe.zotero.zoteroItemsMetadata",
+                options: {
+                    listeners: {
+                        "onCreate.retrieveMetadata": {
+                            funcName: "floe.zotero.zoteroItemsMetadata.retrieveMetadata",
+                            args: ["{zotero}.options.zoteroConfig", "{that}"]
+                        }
+                    }
+                }
+            },
+            zoteroItemsLoader: {
+                type: "fluid.resourceLoader",
+                createOnEvent: "{zoteroItemsMetadata}.events.totalResultsRetrieved",
+                options: {
+                    gradeNames: ["{that}.generateLoaderGrade"],
+                    invokers: {
+                        generateLoaderGrade: {
+                            funcName: "floe.zotero.generateLoaderGrade",
+                            args: ["{zoteroItemsMetadata}.totalResults", "{zotero}.options.zoteroConfig"]
+                        }
+                    }
+                },
+
+            },
+            zoteroItemsParser: {
+                type: "floe.zotero.zoteroItemsParser",
+                createOnEvent: "{zoteroItemsMetadata}.events.totalResultsRetrieved",
+                options: {
+                    listeners: {
+                        "{zoteroItemsLoader}.events.onResourcesLoaded": {
+                            funcName: "floe.zotero.zoteroItemsParser.parse",
+                            args: ["{zoteroItemsLoader}.resources", "{zoteroItemsHolder}.applier", "zoteroItems"]
+                        }
+                    }
+                }
+            },
+            zoteroItemsHolder: {
+                type: "fluid.modelComponent",
+                options: {
+                    model: {
+                        zoteroItems: null
+                    },
+                    modelListeners: {
+                        "zoteroItems": {
+                            "this": "console",
+                            method: "log",
+                            args: "{that}.model.zoteroItems"
+                        }
+                    }
+                }
+            },
+            zoteroItemsPresenter: {
+                type: "fluid.component"
+            }
+        }
+
+    });
+
+// Figures out if we need multiple resources and loads them
+// 50 at a time
+floe.zotero.generateLoaderGrade = function (totalResults, zoteroConfig) {
+
+    var gradeName = "floe.zotero.loaderGrade-" + fluid.allocateGuid();
+
+    var limit = zoteroConfig.limit;
+
+    var totalPages = Math.ceil(totalResults / limit);
+
+    var resourceDefs = {
+    };
+
+    for(var i=1; i<=totalPages; i++) {
+        var start = (i * limit) - limit;
+        var key = "zoteroItems-" + i;
+        var url = fluid.stringTemplate(zoteroConfig.baseUrl + "?format=json&sort=title&limit=%limit&start=%start", {start: start, limit: limit});
+        resourceDefs[key] = url;
+    }
+
+    fluid.defaults(gradeName, {
+        gradeName: ["fluid.component"],
+        resources: resourceDefs
+    });
+
+    return gradeName;
+};
+
+fluid.defaults("floe.zotero.zoteroItemsMetadata", {
+    gradeNames: ["fluid.component"],
+    members: {
+        totalResults: null
+    },
+    events: {
+        totalResultsRetrieved: null
+    }
+});
+
+floe.zotero.zoteroItemsMetadata.retrieveMetadata = function (zoteroConfig, zoteroItemsMetadata) {
+    var url = zoteroConfig.baseUrl + "?limit=1&format=json&sort=title";
+
+    $.ajax({
+        url: url
+    })
+    .done(function (data, textStatus, jqXHR) {
+        var totalResults = jqXHR.getResponseHeader("Total-Results");
+        zoteroItemsMetadata.totalResults = totalResults;
+        zoteroItemsMetadata.events.totalResultsRetrieved.fire();
+    });
+};
+
+fluid.defaults("floe.zotero.zoteroItemsParser", {
+    gradeNames: ["fluid.component"]
+});
+
+floe.zotero.zoteroItemsParser.parse = function (zoteroItemResources, holderApplier, holderEndpoint) {
+    var zoteroItems = [];
+
+    fluid.each(zoteroItemResources, function (zoteroItemResource) {
+
+            var parsedResource = JSON.parse(zoteroItemResource.resourceText);
+
+            zoteroItems = zoteroItems.concat(parsedResource);
+    });
+
+    holderApplier.change(holderEndpoint, zoteroItems);
+
+};
+
+})(jQuery, fluid);
+
+// TODO: deal with large item sizes
---
+++ tests/js/floe-zoteroTests.js
@@ -0,0 +1,71 @@
+/* global fluid, jqUnit, projectTemplate */
+
+(function ($, fluid) {
+
+    "use strict";
+
+    // Basic non-IoC synchronous test
+    jqUnit.test("Test message content", function () {
+        var projectComponent = floe.zotero();
+        jqUnit.expect(0);
+        
+        // jqUnit.assertEquals("Test message has expected content", "Hello, world", projectComponent.model.message);
+    });
+
+    // // Basic non-IoC asyc test
+    // jqUnit.asyncTest("Test message content", function () {
+    //     jqUnit.expect(1);
+    //
+    //     floe.zotero({
+    //         listeners: {
+    //             "onAnnounceComplete.testMessageContent": {
+    //                 "this": "jqUnit",
+    //                 "method": "assertEquals",
+    //                 "args": ["Test message has expected content", "Hello, world", "{that}.model.message"]
+    //             },
+    //             "onAnnounceComplete.testDone": {
+    //                 "this": "jqUnit",
+    //                 "method": "start",
+    //                 "priority": "after:testMessageContent"
+    //             }
+    //         }
+    //     });
+    // });
+    //
+    // // Basic IoC test structure
+    //
+    // fluid.defaults("floe.zoteroTester", {
+    //     gradeNames: ["fluid.test.testCaseHolder"],
+    //     modules: [{
+    //         name: "Test the floe.zotero component.",
+    //         tests: [{
+    //             name: "Test message content and changes.",
+    //             sequence: [{
+    //                 listener: "floe.zoteroTester.testMessageContent",
+    //                 "event": "{projectComponentTest projectComponent}.events.onCreate",
+    //                 args: ["{projectComponent}", "Hello, world"]
+    //             }]
+    //         }]
+    //     }]
+    // });
+    //
+    // fluid.defaults("projectTemplate.tests.projectComponentTest", {
+    //     gradeNames: ["fluid.test.testEnvironment"],
+    //     components: {
+    //         projectComponent: {
+    //             type: "floe.zotero",
+    //             createOnEvent: "{projectComponentTester}.events.onTestCaseStart"
+    //         },
+    //         projectComponentTester: {
+    //             type: "floe.zoteroTester"
+    //         }
+    //     }
+    // });
+    //
+    // floe.zoteroTester.testMessageContent = function (component, expectedMessage) {
+    //     jqUnit.assertEquals("Test message has expected content", expectedMessage, component.model.message);
+    // };
+    //
+    // projectTemplate.tests.projectComponentTest();
+
+})(jQuery, fluid);
